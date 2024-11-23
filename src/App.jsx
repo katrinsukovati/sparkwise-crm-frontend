25b194f5c7c19d201529f92ffa45d3eb36b76ef3
katrinsukovati <katrin.sukovati1@gmail.com>
2024-11-23 14:14:14 -0500

    add routes to the main pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,19 @@
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+// Import the pages
+import DashboardPage from "./pages/DashboardPage/DashboardPage.jsx";
+import ClientsPage from "./pages/ClientsPage/ClientsPage.jsx";
+import CalendarPage from "./pages/CalendarPage/CalendarPage.jsx";
 
 function App() {
   return (
     <BrowserRouter>
-      <Routes></Routes>
+      <Routes>
+        {/* Main pages */}
+        <Route path="/" element={<Navigate to="/dashboard" replace />} />
+        <Route path="/dashboard" element={<DashboardPage />} />
+        <Route path="/clients" element={<ClientsPage />} />
+        <Route path="/calendar" element={<CalendarPage />} />
+      </Routes>
     </BrowserRouter>
   );
 }
