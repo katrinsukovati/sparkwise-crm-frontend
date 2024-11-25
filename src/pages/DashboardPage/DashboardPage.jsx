5fbef0244be51b04341c23a7ae9f8cd37ef491bf
katrinsukovati <katrin.sukovati1@gmail.com>
2024-11-25 13:03:55 -0500

    dashboard structure

--- src/pages/DashboardPage/DashboardPage.jsx
+++ src/pages/DashboardPage/DashboardPage.jsx
@@ -1,7 +1,12 @@
 import "../DashboardPage/DashboardPage.scss";
 
 function DashboardPage() {
-  return <div className="content">I am a dashboard page!</div>;
+  return (
+    <div className="content">
+      <div className="container__upcoming-events box">upcoming events</div>
+      <div className="container__tasks box">tasks</div>
+    </div>
+  );
 }
 
 export default DashboardPage;
