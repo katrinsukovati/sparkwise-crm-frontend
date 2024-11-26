cdd277af827668618d36a66af6648d13fcbb592c
katrinsukovati <katrin.sukovati1@gmail.com>
2024-11-25 22:10:08 -0500

    set up structure of dashboard

--- src/pages/DashboardPage/DashboardPage.jsx
+++ src/pages/DashboardPage/DashboardPage.jsx
@@ -3,8 +3,8 @@ import "../DashboardPage/DashboardPage.scss";
 function DashboardPage() {
   return (
     <div className="content">
-      <div className="container__upcoming-events box">upcoming events</div>
-      <div className="container__tasks box">tasks</div>
+      <div className="container__upcoming-events box">top</div>
+      <div className="container__tasks box">bottom</div>
     </div>
   );
 }
